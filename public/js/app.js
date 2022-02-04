f4206dd51605a3580ff95719c637c6b7af1381bc
Antoniok2 <antoniopiazza18@gmail.com>
2022-02-04 16:47:37 +0100

    create table posts and model, seed, factory

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\ESERCIZI SU LARAVEL\LARAVEL 6\laravel6\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\ESERCIZI SU LARAVEL\LARAVEL 6\laravel6\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\ESERCIZI SU LARAVEL\LARAVEL 6\-laravel-boolpress\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\ESERCIZI SU LARAVEL\LARAVEL 6\-laravel-boolpress\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
